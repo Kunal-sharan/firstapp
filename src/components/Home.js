c65565215fab09f8a6037163c59b2f9bbb42ab4c
Shenron07 <codedragonks07@gmail.com>
2023-04-15 01:17:30 +0530

    Home component

---
+++ src/components/Home.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "../styles/Home.css";
+import dashboard_image from "../assets/dashboard_image.png";
+
+function Home() {
+  return (
+    <div className="home_main_container">
+      <div className="home_button">
+        <p>Just Launched Ghost 3.0</p>
+      </div>
+      <div className="home_heading">
+        <p>
+          Turn your audience <br />{" "}
+          <span className="business">into a business</span>
+        </p>
+      </div>
+      <div className="home_background"></div>
+      <img className="home_image" src={dashboard_image} />
+    </div>
+  );
+}
+
+export default Home;
